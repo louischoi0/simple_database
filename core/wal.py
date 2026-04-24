f72abe1bfb0dd6d6fba3d6445296faa0f720e206
louischoi0 <30791423+louischoi0@users.noreply.github.com>
2026-04-24 15:36:39 +0900

    Update wal.py

--- core/wal.py
+++ core/wal.py
@@ -5,17 +5,19 @@ LAST_LSN = 0
 
 WAL_BUFFER_RING_SIZE = 1024
 WAL_BUFFER_RING = b"\0" * WAL_BUFFER_RING_SIZE
+WAL_BUFFER_OFFSET_LOCK = threading.Lock()
 
 def generate_lsn():
     with LSN_GENERATOR_LOCK:
         LAST_LSN += 1
         return LAST_LSN
 
-
-
-
-
-
+class XLog:
+    def __init__(self, lsn, cmd, payload):
+        self.lsn = lsn
+        self.cmd = cmd
+        self.payload = payload
+    
 
 def bootstrap_wal_system():
     pass
